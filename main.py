997ebe3080f115e8ec89c69cce738d30aa063632
motaALI <motawakil.ali.90@gmail.com>
2023-09-26 10:55:39 +0200

    [UPD] update to check ci

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ def Add(a, b):
         return a + b
         
 def SayHello():
-        print("Hello from circleCI demo")
+        print("Hello from circleCI demo ...!")
 
 if __name__ == '__main__':
         SayHello()
